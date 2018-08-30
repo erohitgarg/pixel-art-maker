fca136b3352d9c4c68d48a81f720c9a4d977dc22
Rohit Garg <garg.rohit.robotics@gmail.com>
2018-08-30 19:51:21 +0530

    feat: User fill color in Grid

--- designs.js
+++ designs.js
@@ -1,52 +1,53 @@
-// Select color input
-// Select size input
-
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
 // Your code goes here!
-const ht=parseInt($("#inputHeight").val()); // select height input
-const wt=parseInt($("#inputWidth").val()); // select width input
-//alert(ht+wt); //tocheck .. @@@@DeleteLater
-let canvas = $('#pixelCanvas')[0];
-//canvas.append('<tr><td> </td></tr>');
+const ht=parseInt($("#inputHeight").val()); // select and initialize height input
+const wt=parseInt($("#inputWidth").val()); // select and initialize width input
+let canvas = $('#pixelCanvas')[0];        // initialize table for creation
+
 let x;
 let y;
+  if(y) {                   // to reset table if already exists
+    for(let i=0;i<ht;i++)
+    {
+      x = canvas.insertRow(0);    // inserts new row
+      for(let j=0;j<wt;j++) {
+        y = x.insertCell(j);     // inserts new cell
+        y.innerHTML="";
+      }
 
-if(y) {
-  for(let i=0;i<ht;i++)
-  {
-    x = canvas.insertRow(0);
-    for(let j=0;j<wt;j++) {
-      y = x.insertCell(j);
-      y.innerHTML="";
     }
-
+  }
+  else {                     // reset table if already exist
+    x=null;
+    y=null;
+    $("#pixelCanvas tr").remove();  // remove table
+    canvas = $('#pixelCanvas')[0];
+    for(let i=0;i<ht;i++)
+    {
+      x = canvas.insertRow(0);
+      for(let j=0;j<wt;j++) {
+        y = x.insertCell(j);
+        y.innerHTML="";
+      }
+    }
   }
 }
-else {
-  x=null;
-  y=null;
-  $("#pixelCanvas tr").remove();
-  let canvas = $('#pixelCanvas')[0];
-  for(let i=0;i<ht;i++)
-  {
-    x = canvas.insertRow(0);
-    for(let j=0;j<wt;j++) {
-      y = x.insertCell(j);
-      y.innerHTML="";
-    }
 
-  }
-}
-
-
-/*let y = x.insertCell(0);
-let z = x.insertCell(1);
-y.innerHTML= "";
-z.innerHTML = "";*/
-}
+//After Submitting Table Size
 $("#sizePicker").submit(function() {
   makeGrid();
-  return false;
+  return false;    // prevents form values reset
+});
+
+// change the mouse to pointer on table hover
+$("#pixelCanvas").hover(function() {
+  $(this).css({"cursor":"pointer"});
+});
+
+// color the clicked grid to the selected color
+$("#pixelCanvas").on('click', 'td',function() {
+  let color = $('#colorPicker').val();   // select color and initialize
+  $(this).css({'background-color':color});
 });
